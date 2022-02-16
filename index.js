6b287a5338e6cf9123b55e2cfd660de580e0bda1
nikitkhakholia <khakholia.nk@gmail.com>
2022-02-17 00:43:15 +0530

    lab11

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root1234',
-  database: 'accounts_module'
+  database: 'mall_accounts'
 });
 connection.connect((err) => {
   if (err) { console.log("DB Connection Failed."); return }
